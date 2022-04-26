371525ab2ba045547748b027c4ac32d46ef465a9
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-04-26 11:35:27 -0400

    create function getRandomNumber

--- 01-color-flipper/hex.js
+++ 01-color-flipper/hex.js
@@ -12,3 +12,7 @@ btn.addEventListener("click", function () {
   color.textContent = hexColor;
   document.body.style.backgroundColor = hexColor;
 });
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * hex.length);
+}
