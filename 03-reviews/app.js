1a1154ed58ced3c4468a8fa43829af10c125174f
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-04-28 11:52:43 -0400

    show person based on item

--- 03-reviews/app.js
+++ 03-reviews/app.js
@@ -59,3 +59,12 @@ window.addEventListener("DOMContentLoaded", function () {
   job.textContent = item.job;
   info.textContent = item.text;
 });
+
+// show person based on item
+function showPerson(person) {
+  const item = reviews[person];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+}
