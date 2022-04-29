60f1217ee392dd821edf728575e55528c204a0f5
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-04-29 12:18:28 -0400

    add conditional statement

--- 04-navbar/app.js
+++ 04-navbar/app.js
@@ -11,6 +11,12 @@ navToggle.addEventListener("click", function () {
   console.log(links.classList);
   console.log(links.classList.contains("random"));
   console.log(links.classList.contains("links"));
+
+  if (links.classList.contains("show-links")) {
+    links.classList.remove("show-links");
+  } else {
+    links.classList.add("show-links");
+  }
   
   
 });
