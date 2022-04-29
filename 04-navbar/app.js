85f8bac86c0d8af81d527b704999f84ae0f5f9c0
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-04-29 12:16:46 -0400

    check links.classList

--- 04-navbar/app.js
+++ 04-navbar/app.js
@@ -6,3 +6,9 @@
 
 const navToggle = document.querySelector(".nav-toggle");
 const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+  console.log(links.classList);
+  
+  
+});
