e192b65907387e993a2605d038833764b7ff3c57
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-05-01 18:47:48 -0400

    add condition

--- 05-sidebar/app.js
+++ 05-sidebar/app.js
@@ -3,5 +3,9 @@ const closeBtn = document.querySelector(".close-btn");
 const sidebar = document.querySelector(".sidebar");
 
 toggleBtn.addEventListener("click", function () {
-  
+  if (sidebar.classList.contains("show-sidebar")) {
+    sidebar.classList.remove("show-sidebar");
+  } else {
+    sidebar.classList.add("show-sidebar");
+  }
 });
