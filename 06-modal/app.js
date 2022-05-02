16be5808aa9a6ff241a39f3ed07217202ccd7368
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-05-02 14:46:25 -0400

    add event listener to modalBtn

--- 06-modal/app.js
+++ 06-modal/app.js
@@ -6,3 +6,7 @@
 const modalBtn = document.querySelector(".modal-btn");
 const modal = document.querySelector(".modal-overlay");
 const closeBtn = document.querySelector(".close-btn");
+
+modalBtn.addEventListener("click", function () {
+  modal.classList.add("open-modal");
+});
