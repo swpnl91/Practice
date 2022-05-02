e8a97d3fa63a3bc8a972e85b539f30a8b457d392
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-05-02 14:46:54 -0400

    add event listener to closeBtn

--- 06-modal/app.js
+++ 06-modal/app.js
@@ -10,3 +10,7 @@ const closeBtn = document.querySelector(".close-btn");
 modalBtn.addEventListener("click", function () {
   modal.classList.add("open-modal");
 });
+
+closeBtn.addEventListener("click", function () {
+  modal.classList.remove("open-modal");
+});
