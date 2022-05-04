dfd959ae950b02b29ae0dc96f4d327b3c80cce63
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-05-03 22:13:05 -0400

    create btn

--- 07-questions/app.js
+++ 07-questions/app.js
@@ -2,5 +2,6 @@
 const questions = document.querySelectorAll(".question");
 
 questions.forEach(function (question) {
-  
+  const btn = question.querySelector(".question-btn");
+  // console.log(btn);
 });
