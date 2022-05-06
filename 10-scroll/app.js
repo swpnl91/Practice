6efaa62c1f3495d651b588e8feabf5b0aca1777c
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-05-06 19:51:16 -0400

    create navToggle

--- 10-scroll/app.js
+++ 10-scroll/app.js
@@ -7,3 +7,6 @@
 // select span
 const date = document.getElementById("date");
 date.innerHTML = new Date().getFullYear();
+
+// ********** close links ************
+const navToggle = document.querySelector(".nav-toggle");
