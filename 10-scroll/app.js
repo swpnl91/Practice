b09641376ef04c711b2946cd2bd210c9d9e5ca7b
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-05-06 19:55:12 -0400

    add event listener for navToggle

--- 10-scroll/app.js
+++ 10-scroll/app.js
@@ -12,3 +12,16 @@ date.innerHTML = new Date().getFullYear();
 const navToggle = document.querySelector(".nav-toggle");
 const linksContainer = document.querySelector(".links-container");
 const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+  // linksContainer.classList.toggle("show-links");
+
+  const linksHeight = links.getBoundingClientRect().height;
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  if (containerHeight === 0) {
+    linksContainer.style.height = `${linksHeight}px`;
+  } else {
+    linksContainer.style.height = 0;
+  }
+  // console.log(linksContainer.getBoundingClientRect());
+});
