d7efd9f432dc1d3ec84b24a36ef530c8e0b999b1
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-05-06 20:01:47 -0400

    add conditions for scroll

--- 10-scroll/app.js
+++ 10-scroll/app.js
@@ -34,5 +34,18 @@ const topLink = document.querySelector(".top-link");
 window.addEventListener("scroll", function () {
   const scrollHeight = window.pageYOffset;
   const navHeight = navbar.getBoundingClientRect().height;
-  
+  if (scrollHeight > navHeight) {
+    navbar.classList.add("fixed-nav");
+  } else {
+    navbar.classList.remove("fixed-nav");
+  }
+  // setup back to top link
+
+  if (scrollHeight > 500) {
+    console.log("helo");
+
+    topLink.classList.add("show-link");
+  } else {
+    topLink.classList.remove("show-link");
+  }
 });
