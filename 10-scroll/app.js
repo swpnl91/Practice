86db2b4f241751b5e7ad7cab05710f9496dbe1d6
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-05-06 20:04:17 -0400

    iterate over scrollLinks

--- 10-scroll/app.js
+++ 10-scroll/app.js
@@ -53,3 +53,7 @@ window.addEventListener("scroll", function () {
 // ********** smooth scroll ************
 // select links
 const scrollLinks = document.querySelectorAll(".scroll-link");
+scrollLinks.forEach((link) => {
+  
+});
+// calculate heights
