f73c715f024a006514588c132926414b18f24916
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-05-09 06:14:11 -0400

    add condition

--- 11-tabs/app.js
+++ 11-tabs/app.js
@@ -4,4 +4,17 @@ const articles = document.querySelectorAll(".content");
 
 about.addEventListener("click", function (e) {
   const id = e.target.dataset.id;
+  if (id) {
+    // remove selected from other buttons
+    btns.forEach(function (btn) {
+      btn.classList.remove("active");
+    });
+    e.target.classList.add("active");
+    // hide other articles
+    articles.forEach(function (article) {
+      article.classList.remove("active");
+    });
+    const element = document.getElementById(id);
+    element.classList.add("active");
+  }
 });
