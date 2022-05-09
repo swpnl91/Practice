9330141cdb146b31c37e09c8fd7645a5ad3d4cbb
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-05-09 06:12:53 -0400

    add event listener to about

--- 11-tabs/app.js
+++ 11-tabs/app.js
@@ -1,3 +1,7 @@
 const about = document.querySelector(".about");
 const btns = document.querySelectorAll(".tab-btn");
 const articles = document.querySelectorAll(".content");
+
+about.addEventListener("click", function (e) {
+  
+});
