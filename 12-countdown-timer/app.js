5d9c96b7d7472d2223d834838782c8dedc4853cd
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-05-10 16:24:38 -0400

    create year

--- 12-countdown-timer/app.js
+++ 12-countdown-timer/app.js
@@ -33,3 +33,7 @@ let tempMonth = tempDate.getMonth();
 let tempDay = tempDate.getDate();
 // months are ZERO index based;
 const futureDate = new Date(tempYear, tempMonth, tempDay + 10, 11, 30, 0);
+
+// let futureDate = new Date(2020, 3, 24, 11, 30, 0);
+
+const year = futureDate.getFullYear();
