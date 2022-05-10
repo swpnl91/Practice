c61bf63b174d038cfc2143aa187d0e0d9c31cd67
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-05-10 16:26:17 -0400

    create month

--- 12-countdown-timer/app.js
+++ 12-countdown-timer/app.js
@@ -39,3 +39,6 @@ const futureDate = new Date(tempYear, tempMonth, tempDay + 10, 11, 30, 0);
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
 const minutes = futureDate.getMinutes();
+
+let month = futureDate.getMonth();
+month = months[month];
