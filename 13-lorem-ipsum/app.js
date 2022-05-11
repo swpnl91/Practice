cb24b4d496c8f63704ffc8d99873e93d46c193b8
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-05-11 18:28:27 -0400

    create event listener for form

--- 13-lorem-ipsum/app.js
+++ 13-lorem-ipsum/app.js
@@ -14,3 +14,9 @@ I just told you! You've killed me! Fry! Quit doing the right thing, you jerk! Mi
 const form = document.querySelector(".lorem-form");
 const amount = document.getElementById("amount");
 const result = document.querySelector(".lorem-text");
+
+form.addEventListener("submit", function (e) {
+  // A click on a form submit button – initiates its submission to the server.
+
+  
+});
