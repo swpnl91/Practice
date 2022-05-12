19604c7c87d44fdb5358f32a336a0c77365dc4e3
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-05-12 07:07:45 -0400

    create submitBtn

--- 14-grocery-bud/app.js
+++ 14-grocery-bud/app.js
@@ -3,3 +3,4 @@
 const form = document.querySelector(".grocery-form");
 const alert = document.querySelector(".alert");
 const grocery = document.getElementById("grocery");
+const submitBtn = document.querySelector(".submit-btn");
