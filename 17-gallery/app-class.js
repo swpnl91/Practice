b2878e7ce7067414eff7e5da6a01d6451f5fc9f0
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-05-16 07:15:25 -0400

    add constructo

--- 17-gallery/app-class.js
+++ 17-gallery/app-class.js
@@ -9,5 +9,34 @@ function getElement(selection) {
 }
 
 class Gallery {
-  
+  constructor(element) {
+    this.container = element;
+    this.list = [...element.querySelectorAll('.img')];
+    // target
+    this.modal = getElement('.modal');
+    this.modalImg = getElement('.main-img');
+    this.imageName = getElement('.image-name');
+    this.modalImages = getElement('.modal-images');
+    this.closeBtn = getElement('.close-btn');
+    this.nextBtn = getElement('.next-btn');
+    this.prevBtn = getElement('.prev-btn');
+    // self ref
+    // let self = this;
+    // bind functions
+    // this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+    this.nextImage = this.nextImage.bind(this);
+    this.prevImage = this.prevImage.bind(this);
+    this.chooseImage = this.chooseImage.bind(this);
+    // container event
+    this.container.addEventListener(
+      'click',
+      function (e) {
+        // self.openModal();
+        if (e.target.classList.contains('img')) {
+          this.openModal(e.target, this.list);
+        }
+      }.bind(this)
+    );
+  }
 }
