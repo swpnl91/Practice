3610b283f110fd14404b616e97c59a61a1726420
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-05-23 08:11:30 -0400

    create increment

--- 18-numbers/app.js
+++ 18-numbers/app.js
@@ -2,4 +2,6 @@ const items = [...document.querySelectorAll('.number')];
 
 const updateCount = (el) => {
   const value = parseInt(el.dataset.value);
+  const increment = Math.ceil(value / 1000);
+  // const increment = 1;
 };
