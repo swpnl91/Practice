7c611a64e4bf098e20f197d162d48543b00db109
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-05-25 12:46:44 -0400

    return data from function

--- 19-dark-mode/app.js
+++ 19-dark-mode/app.js
@@ -9,4 +9,16 @@ const articlesData = articles
   .map((article) => {
     const { title, date, length, snippet } = article;
     const formatDate = moment(date).format('MMMM Do, YYYY');
+
+    return `<article class="post">
+          <h2>${title}</h2>
+          <div class="post-info">
+            <span>${formatDate}</span>
+            <span>${length} min read</span>
+          </div>
+          <p>
+            ${snippet}
+          </p>
+        </article>`;
   })
+  .join('');
