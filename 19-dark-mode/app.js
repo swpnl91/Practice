c0815dca871ab535261299a686195c140f5ad2fe
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-05-25 12:39:41 -0400

    add event listener to toggleBtn

--- 19-dark-mode/app.js
+++ 19-dark-mode/app.js
@@ -1,2 +1,6 @@
 const toggleBtn = document.querySelector('.btn');
 const articlesContainer = document.querySelector('.articles');
+
+toggleBtn.addEventListener('click', () => {
+  document.documentElement.classList.toggle('dark-theme');
+});
