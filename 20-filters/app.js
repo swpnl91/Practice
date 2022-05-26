3153b90cac8199ae92c239bfc5f6f41ebff7a833
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-05-26 08:20:35 -0400

    add condition

--- 20-filters/app.js
+++ 20-filters/app.js
@@ -3,7 +3,10 @@ let filteredProducts = [...products];
 const productsContainer = document.querySelector('.products-container');
 
 const displayProducts = () => {
-  
+  if (filteredProducts.length < 1) {
+    productsContainer.innerHTML = `<h6>Sorry, no products matched your search</h6>`;
+    return;
+  }
 
   
 };
