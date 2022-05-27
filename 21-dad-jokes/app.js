339c89cae60046bb1b373860059b6771e884a31e
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-05-27 07:34:55 -0400

    add event listener to btn

--- 21-dad-jokes/app.js
+++ 21-dad-jokes/app.js
@@ -2,3 +2,7 @@ const url = 'https://icanhazdadjoke.com/';
 
 const btn = document.querySelector('.btn');
 const result = document.querySelector('.result');
+
+btn.addEventListener('click', () => {
+  fetchDadJoke();
+});
