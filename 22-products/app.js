e16b0242cd12380db9539bcaf60cef236df8dea7
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-05-30 09:30:11 -0400

    create fetchProducts

--- 22-products/app.js
+++ 22-products/app.js
@@ -1,3 +1,14 @@
 const url = 'https://course-api.com/javascript-store-products';
 
 const productsDOM = document.querySelector('.products-center');
+
+const fetchProducts = async () => {
+  productsDOM.innerHTML = '<div class="loading"></div>';
+  try {
+    const resp = await fetch(url);
+    const data = await resp.json();
+    return data;
+  } catch (error) {
+    productsDOM.innerHTML = '<p class="error">there was an error</p>';
+  }
+};
