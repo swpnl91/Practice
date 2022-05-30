dd88e787fde7919d1a1de40f9b86cbe0571588d1
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-05-30 09:31:23 -0400

    create displayProducts

--- 22-products/app.js
+++ 22-products/app.js
@@ -12,3 +12,26 @@ const fetchProducts = async () => {
     productsDOM.innerHTML = '<p class="error">there was an error</p>';
   }
 };
+
+const displayProducts = (list) => {
+  const productList = list
+    .map((product) => {
+      const { id } = product;
+      const { name: title, price } = product.fields;
+      const { url: img } = product.fields.image[0];
+      const formatPrice = price / 100;
+      // id,name,price,img
+      return `<a class="single-product" href="product.html?id=${id}&name=john&age=25">
+            <img src="${img}" class="single-product-img img" alt="${title}" />
+            <footer>
+              <h5 class="name">${title}</h5>
+              <span class="price">$${formatPrice}</span>
+            </footer>
+          </a>`;
+    })
+    .join('');
+  productsDOM.innerHTML = ` <div class="products-container">
+         ${productList}
+          
+        </div>`;
+};
