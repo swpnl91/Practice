3c3b40350d612a7ad58300474d092c6cdc308e7a
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-06-01 14:18:49 -0400

    create showUser

--- 23-random-user/app.js
+++ 23-random-user/app.js
@@ -3,3 +3,11 @@ import getUser from './utils/fetchUser.js';
 import displayUser from './utils/displayUser.js';
 
 const btn = get('.btn');
+
+const showUser = async () => {
+  // get user from api
+  const person = await getUser();
+  displayUser(person);
+
+  // display user
+};
