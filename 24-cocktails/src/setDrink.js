ee582ae6554523172fed531d59b5d1d34078dbaa
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-06-02 14:31:41 -0400

    create setDrink

---
+++ 24-cocktails/src/setDrink.js
@@ -0,0 +1,8 @@
+const setDrink = (section) => {
+  section.addEventListener('click', function (e) {
+    // e.preventDefault();
+    const id = e.target.parentElement.dataset.id;
+    // JSON.stringify JSON.parse
+    localStorage.setItem('drink', id);
+  });
+};
