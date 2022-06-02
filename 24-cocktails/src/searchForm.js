2992483439dd97e0a5fad4cdf6592dc6569cbd45
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-06-02 14:30:42 -0400

    add event listener to form

--- 24-cocktails/src/searchForm.js
+++ 24-cocktails/src/searchForm.js
@@ -5,3 +5,10 @@ const baseURL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 
 const form = get('.search-form');
 const input = get('[name="drink"]');
+
+form.addEventListener('keyup', function (e) {
+  e.preventDefault();
+  const value = input.value;
+  if (!value) return;
+  presentDrinks(`${baseURL}${value}`);
+});
