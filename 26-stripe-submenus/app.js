b8500c1669889310af9537ea3ffec5aa7ac08d31
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-06-05 14:06:29 -0400

    create sidebarWrapper

--- 26-stripe-submenus/app.js
+++ 26-stripe-submenus/app.js
@@ -2,3 +2,4 @@ import sublinks from './data.js';
 
 const toggleBtn = document.querySelector('.toggle-btn');
 const closeBtn = document.querySelector('.close-btn');
+const sidebarWrapper = document.querySelector('.sidebar-wrapper');
