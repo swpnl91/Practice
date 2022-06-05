b2b2e4d8d64a9f1f55bdf016f85fca1a3ab8971f
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-06-05 14:12:16 -0400

    iterate through linkBtns

--- 26-stripe-submenus/app.js
+++ 26-stripe-submenus/app.js
@@ -34,3 +34,40 @@ ${links
 </article>`;
   })
   .join('');
+
+linkBtns.forEach((btn) => {
+  btn.addEventListener('mouseover', function (e) {
+    const text = e.currentTarget.textContent;
+    const tempBtn = e.currentTarget.getBoundingClientRect();
+    const center = (tempBtn.left + tempBtn.right) / 2;
+    const bottom = tempBtn.bottom - 3;
+
+    const tempPage = sublinks.find((link) => link.page === text);
+    if (tempPage) {
+      const { page, links } = tempPage;
+      submenu.classList.add('show');
+      submenu.style.left = `${center}px`;
+      submenu.style.top = `${bottom}px`;
+      // OPTIONAL
+      let columns = 'col-2';
+      if (links.length === 3) {
+        columns = 'col-3';
+      }
+      if (links.length > 3) {
+        columns = 'col-4';
+      }
+      submenu.innerHTML = `
+      <section> 
+      <h4>${page}</h4>
+      <div class="submenu-center ${columns}">
+      ${links
+        .map((link) => {
+          return `<a href="${link.url}"><i class="${link.icon}"></i>${link.label}</a>`;
+        })
+        .join('')}
+      </div>
+      </section>
+      `;
+    }
+  });
+});
