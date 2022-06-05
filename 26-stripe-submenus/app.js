1e95bf6ae36f6353cce273ab10b35ec50474c53c
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-06-05 14:06:58 -0400

    create sidebar

--- 26-stripe-submenus/app.js
+++ 26-stripe-submenus/app.js
@@ -3,3 +3,4 @@ import sublinks from './data.js';
 const toggleBtn = document.querySelector('.toggle-btn');
 const closeBtn = document.querySelector('.close-btn');
 const sidebarWrapper = document.querySelector('.sidebar-wrapper');
+const sidebar = document.querySelector('.sidebar-links');
