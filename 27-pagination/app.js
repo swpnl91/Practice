25ac0c918af2f217609ffb17d84fe3ccf301d34f
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-06-06 13:04:28 -0400

    create index

--- 27-pagination/app.js
+++ 27-pagination/app.js
@@ -5,3 +5,5 @@ import displayButtons from './displayButtons.js'
 
 const title = document.querySelector('.section-title h1')
 const btnContainer = document.querySelector('.btn-container')
+
+let index = 0
