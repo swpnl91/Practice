4c818faba3711782f0c2968c11a0d2bf76b5389d
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-06-06 13:03:37 -0400

    create title

--- 27-pagination/app.js
+++ 27-pagination/app.js
@@ -2,3 +2,5 @@ import fetchFollowers from './fetchFollowers.js'
 import displayFollowers from './displayFollowers.js'
 import paginate from './paginate.js'
 import displayButtons from './displayButtons.js'
+
+const title = document.querySelector('.section-title h1')
