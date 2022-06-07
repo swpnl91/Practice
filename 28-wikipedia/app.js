175b7ea6e6a931d2e3231075c249d4fbced5bb8d
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-06-07 12:18:40 -0400

    create inputDOM

--- 28-wikipedia/app.js
+++ 28-wikipedia/app.js
@@ -1,3 +1,4 @@
 const url = 'https://en.wikipedia.org/w/api.php?action=query&list=search&srlimit=20&format=json&origin=*&srsearch=';
 
 const formDOM = document.querySelector('.form');
+const inputDOM = document.querySelector('.form-input');
