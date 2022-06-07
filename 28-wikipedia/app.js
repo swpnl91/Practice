0ee93533b28d1455039849af95c1762b3db70c32
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-06-07 12:19:58 -0400

    add event listener to formDOM

--- 28-wikipedia/app.js
+++ 28-wikipedia/app.js
@@ -3,3 +3,14 @@ const url = 'https://en.wikipedia.org/w/api.php?action=query&list=search&srlimit
 const formDOM = document.querySelector('.form');
 const inputDOM = document.querySelector('.form-input');
 const resultsDOM = document.querySelector('.results');
+
+formDOM.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const value = inputDOM.value;
+  if (!value) {
+    resultsDOM.innerHTML =
+      '<div class="error"> please enter valid search term</div>';
+    return;
+  }
+  fetchPages(value);
+});
